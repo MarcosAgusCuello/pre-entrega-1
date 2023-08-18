7a93527d14196803de11b6f63b47e721e0b523d0
Marcos Cuello <cuellomarcos2017@gmail.com>
2023-08-18 14:55:18 -0300

    pre-entrega final 1

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import prodsRouter from './routes/products.routes.js';
+import cartsRouter from './routes/carts.routes.js'; 
+
+
+
+const app = express()
+const PORT = 8080
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('/api/products', prodsRouter)
+app.use('/api/carts', cartsRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
